5c126e95b1783de15a687e2caeeefd24c13723a0
Dibakar <dibakarbera01@gmail.com>
2024-03-15 13:21:25 +0530

    fisrt commit

---
+++ Arrays/twosum.java
@@ -0,0 +1,8 @@
+/*
+ Given an array of size N and integer target. Fins the indices (i,j) of two numbers such that their sum is to target. (i!=j).
+ sample input:-
+ N=5
+ arr = {7,6,3,8,2}
+ target=14
+ sample output :- 1 3
+*/
